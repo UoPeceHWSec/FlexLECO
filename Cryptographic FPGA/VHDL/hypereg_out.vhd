c014c6a42da053e0aca06551380a25889ce16446
Athanassios Moschos <ece7567@upnet.gr>
2018-05-01 16:04:57 +0300

    Updated licensed files

--- Cryptographic FPGA/VHDL/hypereg_out.vhd
+++ Cryptographic FPGA/VHDL/hypereg_out.vhd
@@ -1,13 +1,30 @@
 ---------------------------------------------------------------------------
+--	Copyright 2018 Athanasios Moschos, Apostolos Fournaris
+--
+--	Licensed under the Apache License, Version 2.0 (the "License");
+--	you may not use this file except in compliance with the License.
+--	You may obtain a copy of the License at
+--
+--  	  http://www.apache.org/licenses/LICENSE-2.0
+--
+--	Unless required by applicable law or agreed to in writing, software
+--	distributed under the License is distributed on an "AS IS" BASIS,
+--	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--	See the License for the specific language governing permissions and
+--	limitations under the License.
+--	
+--	 When you publish any results arising from the use of this code, we will
+--   appreciate it if you can cite our webpage.
+--	 (https://github.com/UoPeceHWSec/FlexLECO)
+---------------------------------------------------------------------------
+---------------------------------------------------------------------------
 -- HyperegOUT component - Crypto FPGA Side
 --                                   
 -- File name   : hypereg_out.vhd
 -- Version     : Version  beta 1.0
 -- Created     : 1/JAN/2016
 -- Last update : 7/JAN/2016
--- Desgined by : Athanassios Moschos
---
---Copyright (C) 2016 Athanassios Moschos
+-- Desgined by : Athanasios Moschos
 ---------------------------------------------
 library IEEE;
 use IEEE.NUMERIC_STD.ALL;
